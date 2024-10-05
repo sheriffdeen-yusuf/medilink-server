75e0866bd5c600886ba7ef8b3507a35fdaaa24aa
sheriffdeen-yusuf <Sheriffdeenyusuf1130@gmail.com>
2024-10-05 14:46:14 +0100

    added frontend deploy url

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 
 const corsOptions = {
-  origin: ['http://localhost:3000'],
+  origin: ['http://localhost:3000', 'https://medical-link.netlify.app/'],
   credentials: true,
 };
 
